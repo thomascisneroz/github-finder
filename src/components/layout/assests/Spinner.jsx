520eff87775d6405cc3aa422421f04bb8d9f94ca
ThomasCisneroz <thomasrcisnseroz@gmail.com>
2022-05-10 13:13:17 -0400

    User list and loading gif

---
+++ src/components/layout/assests/Spinner.jsx
@@ -0,0 +1,15 @@
+import coolspinner from '../assests/coolspinner.gif';
+
+function Spinner () {
+    return 
+    <div className='w-100 mt-20'>
+        <img 
+        width={180}
+        className='text-center mx-auto'
+        src={coolspinner}
+        alt='Loading...'  
+        />
+    </div>
+}
+
+export default Spinner;
